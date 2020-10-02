f21fe46013cea1a8e3617d0b65254a47bb6e29ee
Marina Poprotskaya <mitsevich.marina@gmail.com>
2020-10-02 17:14:03 +0300

    refactor: move logic in a separate function

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ class App extends React.Component {
     this.onDeleted = this.onDeleted.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
     this.onCorrect = this.onCorrect.bind(this);
+    this.onDown = this.onDown.bind(this);
+    this.onUp = this.onUp.bind(this);
+    this.getCurrentSearchingIndex = this.getCurrentSearchingIndex.bind(this);
 
     this.state = {
       todoList: JSON.parse(localStorage.getItem("todoList")) || [],
@@ -19,9 +22,15 @@ class App extends React.Component {
     };
   }
 
-  onDeleted = (id) => {
+  getCurrentSearchingIndex = (id) => {
     const { todoList } = this.state;
     const searchingIndex = todoList.findIndex((el) => el.id === id);
+    return searchingIndex;
+  }
+
+  onDeleted = (id) => {
+    const { todoList } = this.state;
+    const searchingIndex = this.getCurrentSearchingIndex(id);
     const beforeIndex = todoList.slice(0, searchingIndex);
     const afterIndex = todoList.slice(searchingIndex + 1);
     const newTodoList = [...beforeIndex, ...afterIndex];
@@ -40,12 +49,13 @@ class App extends React.Component {
     const newTodoList = [...todoList, itemTemplate];
     localStorage.setItem("todoList", JSON.stringify(newTodoList));
     this.setState({ todoList: newTodoList });
+    e.target.childNodes[0].focus()
     e.target.reset();
   };
 
   onCorrect = (id) => {
     const { todoList } = this.state;
-    const searchingIndex = todoList.findIndex((el) => el.id === id);
+   const searchingIndex = this.getCurrentSearchingIndex(id);
     this.setState({ 
       showModal: !this.state.showModal,
       value: todoList[searchingIndex].content,
@@ -63,12 +73,27 @@ class App extends React.Component {
     });
   };
 
+  onUp = (id) => {
+  const searchingIndex = this.getCurrentSearchingIndex(id);
+  if(searchingIndex === 0) return;
+  console.log('up', id);
+  }
+
+  onDown = (id) => {
+    const { todoList } = this.state;
+    const searchingIndex = this.getCurrentSearchingIndex(id)
+    if(searchingIndex === todoList.length-1) return;
+    console.log('down', id);
+  }
+
   render() {
     const elements = this.state.todoList.map((item) => {
       return (
         <Item
           onDeleted={this.onDeleted}
           onCorrect={this.onCorrect}
+          onUp={this.onUp}
+          onDown={this.onDown}
           content={item.content}
           id={item.id}
           key={item.id}
--- src/components/list-item/component.jsx
+++ src/components/list-item/component.jsx
@@ -3,13 +3,13 @@ import "./style.css";
 
 class Item extends React.Component {
 
-render() {
+render() { 
     return (
           <div className="list-item" >
             <div className="content">{this.props.content}</div>
             <div className="buttons-wrapper">
-              <button className="btn up"></button>
-              <button className="btn down"></button>
+              <button onClick={() => this.props.onUp(this.props.id)} className="btn up"></button>
+              <button onClick={() => this.props.onDown(this.props.id)} className="btn down"></button>
               <button onClick={() => this.props.onCorrect(this.props.id)} className="btn fix"></button>
               <button onClick={() => this.props.onDeleted(this.props.id)} className="btn del"></button>
             </div>
