a56919280c020115ab3f0e026c77746265294d49
Marina Poprotskaya <mitsevich.marina@gmail.com>
2020-10-05 12:41:37 +0300

    refactor: move functions from onClick event

--- src/components/list-item/component.jsx
+++ src/components/list-item/component.jsx
@@ -2,24 +2,35 @@ import React from "react";
 import "./style.css";
 
 class Item extends React.Component {
+  handleUp = () => {
+    this.props.onUp(this.props.id);
+  };
 
-render() { 
+  handleDown = () => {
+    this.props.onDown(this.props.id);
+  };
+
+  handleCorrect = () => {
+    this.props.onCorrect(this.props.id);
+  };
+
+  handleDelete = () => {
+    this.props.onDelete(this.props.id);
+  };
+
+  render() {
     return (
-          <div className="list-item" >
-            <div className="content">{this.props.content}</div>
-            <div className="buttons-wrapper">
-              <button onClick={() => this.props.onUp(this.props.id)} className="btn up"></button>
-              <button onClick={() => this.props.onDown(this.props.id)} className="btn down"></button>
-              <button onClick={() => this.props.onCorrect(this.props.id)} className="btn fix"></button>
-              <button onClick={() => this.props.onDelete(this.props.id)} className="btn del"></button>
-            </div>
-          </div>
+      <div className="list-item">
+        <div className="content">{this.props.content}</div>
+        <div className="buttons-wrapper">
+          <button onClick={this.handleUp} className="btn up"></button>
+          <button onClick={this.handleDown} className="btn down"></button>
+          <button onClick={this.handleCorrect} className="btn fix"></button>
+          <button onClick={this.handleDelete} className="btn del"></button>
+        </div>
+      </div>
     );
+  }
 }
-}
-
-// handleUp = () => {
-//   this.props.onUp(this.props.id)
-//   }
 
 export default Item;
